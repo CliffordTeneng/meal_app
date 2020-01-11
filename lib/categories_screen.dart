045821778788ad6a721eaf2fbcdcbd6dc687483e
cliffordTeneng <tenengclifford5@gmail.com>
2020-01-11 15:30:49 +0100

    implemented the category screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+import 'package:meals_app/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[
-        
-      ],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3/2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("DeliMeal"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(right:8.0, left: 8),
+        child: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                catData.title,
+                catData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 20,
+        ),
+    ),
       ),
     );
   }
