939cadafff92d058fe3946a5df349906a810bd37
cliffordTeneng <tenengclifford5@gmail.com>
2020-01-17 21:15:25 +0100

    added data in the dummy data file

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -6,7 +6,7 @@ enum Complexity {
   Hard,
 }
 
-enum Affordabilty {
+enum Affordability {
   Affordable,
   Pricey,
   Luxurious,
@@ -14,30 +14,32 @@ enum Affordabilty {
 
 class Meal {
   final String id;
-  final List<String> categoies;
+  final List<String> categories;
   final String title;
   final String imageUrl;
   final List<String> ingredients;
   final List<String> steps;
   final int duration;
   final Complexity complexity;
-  final Affordabilty affordabilty;
+  final Affordability affordability;
   final bool isLactoseFree;
   final bool isVegan;
-  final bool isVegeterian;
+  final bool isVegetarian;
+  final bool isGlutenFree;
 
  const Meal({
     @required this.id,
-    @required this.categoies,
+    @required this.categories,
     @required this.title,
     @required this.imageUrl,
     @required this.ingredients,
     @required this.steps,
     @required this.duration,
     @required this.complexity,
-    @required this.affordabilty,
+    @required this.affordability,
     @required this.isLactoseFree,
     @required this.isVegan,
-    @required this.isVegeterian,
+    @required this.isVegetarian, 
+    @required this.isGlutenFree,
   });
 }
