1d5d011e147605cc02e00b17e788f41abf97e774
cliffordTeneng <tenengclifford5@gmail.com>
2020-01-17 21:29:31 +0100

    grouping of files

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routName = '/category-meals';
@@ -12,14 +13,18 @@ class CategoryMealsScreen extends StatelessWidget {
     final routeArguments = ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArguments['title'];
     final categoryId = routeArguments['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal){
+      return meal.categories.contains(categoryId);
+    }).toList();
+
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
       body:ListView.builder(itemBuilder: (ctx, index){
-
-      }, itemCount: ,),
+        return Text(categoryMeals[index].title,);
+      }, itemCount: categoryMeals.length,),
     );
   }
 }
